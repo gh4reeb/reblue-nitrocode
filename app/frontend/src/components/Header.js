2f95403221fcfb09a68a22bbc47a0ec4138ca735
Adil Shafiq <34338792+gh4reeb@users.noreply.github.com>
2025-02-22 18:51:54 +0500

    Update Header.js

--- app/frontend/src/components/Header.js
+++ app/frontend/src/components/Header.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+function Header() {
+  return (
+    <header className="header">
+      <nav>
+        <ul>
+          <li><Link to="/">Dashboard</Link></li>
+          <li><Link to="/repos">Repositories</Link></li>
+          <li><Link to="/pipeline">Pipeline</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
