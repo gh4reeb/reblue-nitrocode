9471341ea796e36dafeb9491b5c5c80da6175432
Adil Shafiq <34338792+gh4reeb@users.noreply.github.com>
2025-02-22 18:49:53 +0500

    Update vaultHelper.js

--- app/backend/utils/vaultHelper.js
+++ app/backend/utils/vaultHelper.js
@@ -1,1 +1,13 @@
+const axios = require('axios');
+const { VAULT_ADDR, VAULT_TOKEN } = require('../config');
 
+exports.getSecret = async (secretPath) => {
+  try {
+    const response = await axios.get(`${VAULT_ADDR}/v1/${secretPath}`, {
+      headers: { 'X-Vault-Token': VAULT_TOKEN },
+    });
+    return response.data.data;
+  } catch (error) {
+    throw new Error(`Vault error: ${error.message}`);
+  }
+};
