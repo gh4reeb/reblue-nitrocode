f072f9d9ad9083c23580c0ef4a03c5403147cd4d
Adil Shafiq <34338792+gh4reeb@users.noreply.github.com>
2025-02-22 18:46:21 +0500

    Update repoRoutes.js

--- app/backend/routes/repoRoutes.js
+++ app/backend/routes/repoRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const { getRepositories } = require('../controllers/repoController');
 
+const router = express.Router();
+
+router.get('/', getRepositories);
+
+module.exports = router;
